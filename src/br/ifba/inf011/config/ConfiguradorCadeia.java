798d55f43339e20bf4e0d4eedc6e76ceb6575d24
m310296 <jaqueline.costa@capgemini.com>
2025-03-06 11:43:16 -0300

    master - feat: Configurador de cadeia refatorado.

--- src/br/ifba/inf011/config/ConfiguradorCadeia.java
+++ src/br/ifba/inf011/config/ConfiguradorCadeia.java
@@ -1,32 +1,65 @@
 package br.ifba.inf011.config;
 
-import br.ifba.inf011.handler.*;
-import br.ifba.inf011.strategy.*;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import br.ifba.inf011.handler.Handler;
+import br.ifba.inf011.handler.HandlerPrioridadeAlta;
+import br.ifba.inf011.handler.HandlerPrioridadeBaixa;
+import br.ifba.inf011.handler.HandlerPrioridadeMedia;
+import br.ifba.inf011.strategy.EstrategiaNotificacao;
+import br.ifba.inf011.strategy.NotificacaoEmail;
+import br.ifba.inf011.strategy.NotificacaoRedeSocial;
+import br.ifba.inf011.strategy.NotificacaoSMS;
+import br.ifba.inf011.strategy.NotificacaoWhatsApp;
+
 public class ConfiguradorCadeia {
-    
+
     public static List<Handler> configurarCadeia() {
-        List<Handler> handlers =new ArrayList<>();
+        EstrategiaNotificacao email = criarNotificacaoEmail();
+        EstrategiaNotificacao whatsApp = criarNotificacaoWhatsApp();
+        EstrategiaNotificacao sms = criarNotificacaoSMS();
+        EstrategiaNotificacao redeSocial = criarNotificacaoRedeSocial();
 
-        
-        EstrategiaNotificacao email =new NotificacaoEmail();
-        EstrategiaNotificacao whatsApp =new NotificacaoWhatsApp();
-        EstrategiaNotificacao sms =new NotificacaoSMS();
-        EstrategiaNotificacao redeSocial =new NotificacaoRedeSocial();
+        Handler alta = criarHandlerAlta(email, whatsApp, sms);
+        Handler media = criarHandlerMedia(email, redeSocial);
+        Handler baixa = criarHandlerBaixa(email);
 
-        
-        Handler alta = new HandlerPrioridadeAlta(email, whatsApp, sms);
-        Handler media = new HandlerPrioridadeMedia(email, redeSocial);
-        Handler baixa = new HandlerPrioridadeBaixa(email); 
+        configurarCadeiaHandlers(alta, media, baixa);
 
-        
+        return Arrays.asList(alta);
+    }
+
+    private static EstrategiaNotificacao criarNotificacaoEmail() {
+        return new NotificacaoEmail();
+    }
+
+    private static EstrategiaNotificacao criarNotificacaoWhatsApp() {
+        return new NotificacaoWhatsApp();
+    }
+
+    private static EstrategiaNotificacao criarNotificacaoSMS() {
+        return new NotificacaoSMS();
+    }
+
+    private static EstrategiaNotificacao criarNotificacaoRedeSocial() {
+        return new NotificacaoRedeSocial();
+    }
+
+    private static Handler criarHandlerAlta(EstrategiaNotificacao email, EstrategiaNotificacao whatsApp, EstrategiaNotificacao sms) {
+        return new HandlerPrioridadeAlta(email, whatsApp, sms);
+    }
+
+    private static Handler criarHandlerMedia(EstrategiaNotificacao email, EstrategiaNotificacao redeSocial) {
+        return new HandlerPrioridadeMedia(email, redeSocial);
+    }
+
+    private static Handler criarHandlerBaixa(EstrategiaNotificacao email) {
+        return new HandlerPrioridadeBaixa(email);
+    }
+
+    private static void configurarCadeiaHandlers(Handler alta, Handler media, Handler baixa) {
         alta.setProximo(media);
         media.setProximo(baixa);
-
-        handlers.add(alta);
-        return handlers;
     }
 }
